08dc7610f5d083a30e106187323b50846a37a8ba
devtronaut <69691214+devtronaut@users.noreply.github.com>
2024-10-05 23:53:59 +0200

    Initial commit (#1)
    * Add CODEOWNERS file
    
    * Initial commit
    
    * chore: format and lint
    
    * chore: add codeowners

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
+    title: 'Tablehub website',
+    description:
+        'Website for tablehub. Showcase and information for interested clubs.',
+}
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode
+}>) {
+    return (
+        <html lang="de">
+            <body className={`font-normal antialiased`}>{children}</body>
+        </html>
+    )
+}
