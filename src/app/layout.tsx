36d67e8a5cad995ceb8c34f662de821308a491e0
Fabian Diemand <fabian.diemand@hotmail.com>
2024-10-06 23:41:11 +0200

    feature(favicon): add favicons, create manifest and public directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import './globals.css'
 export const metadata: Metadata = {
     title: 'Tablehub',
     description:
-        'Website for tablehub. Showcase and information for interested clubs.',
+        'Website for Tablehub. Showcase and information for interested clubs.',
 }
 
 export default async function RootLayout({
@@ -19,6 +19,26 @@ export default async function RootLayout({
 
     return (
         <html lang={locale}>
+            <head>
+                {/* The root for the favicon import refers to the 'public' directory */}
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/favicons/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicons/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicons/favicon-16x16.png"
+                />
+            </head>
             <body
                 className={`font-normal antialiased dark:bg-backgroundDark bg-backgroundLight dark:text-white text-black`}
             >
---
+++ src/app/manifest.ts
@@ -0,0 +1,30 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: 'Tablehub',
+        short_name: 'Tablehub',
+        description: 'Website for Tablehub. Showcase and information for interested clubs.',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#fff',
+        theme_color: '#fff',
+        icons: [
+            // The root for the favicon import refers to the 'public' directory
+            {
+                src: "/favicons/android-chrome-192x192.png",
+                sizes: "192x192",
+                type: "image/png",
+            },
+            {
+                src: "/favicons/android-chrome-512x512.png",
+                sizes: "512x512",
+                type: "image/png"
+            },
+            {
+                src: "/favicons/apple-touch-icon.png",
+                type: "image/png",
+            }
+        ],
+    }
+}
