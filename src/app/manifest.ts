f78644e32f42834e82763f31e0290ab6b9a14c83
Fabian Diemand <fabian.diemand@hotmail.com>
2024-10-07 00:57:30 +0200

    chore: add vscode settings and format

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -4,7 +4,8 @@ export default function manifest(): MetadataRoute.Manifest {
     return {
         name: 'Tablehub',
         short_name: 'Tablehub',
-        description: 'Website for Tablehub. Showcase and information for interested clubs.',
+        description:
+            'Website for Tablehub. Showcase and information for interested clubs.',
         start_url: '/',
         display: 'standalone',
         background_color: '#fff',
@@ -12,19 +13,19 @@ export default function manifest(): MetadataRoute.Manifest {
         icons: [
             // The root for the favicon import refers to the 'public' directory
             {
-                src: "/favicons/android-chrome-192x192.png",
-                sizes: "192x192",
-                type: "image/png",
+                src: '/favicons/android-chrome-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
             },
             {
-                src: "/favicons/android-chrome-512x512.png",
-                sizes: "512x512",
-                type: "image/png"
+                src: '/favicons/android-chrome-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
             },
             {
-                src: "/favicons/apple-touch-icon.png",
-                type: "image/png",
-            }
+                src: '/favicons/apple-touch-icon.png',
+                type: 'image/png',
+            },
         ],
     }
 }
