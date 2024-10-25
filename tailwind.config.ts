a9d12360db132a219a40d2f6bf49e126db7fae3a
Fabian Diemand <fabian.diemand@hotmail.com>
2024-10-25 16:48:11 +0200

    feature(icons): add animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from 'tailwindcss'
 import daisyui from 'daisyui'
+import { transform } from 'next/dist/build/swc'
 
 const config: Config = {
     darkMode: 'selector',
@@ -22,6 +23,33 @@ const config: Config = {
                 cardLight: '#fafcfc',
                 shimmer: '#36c9b9'
             },
+            animation: {
+                wiggle: 'wiggle 2s ease infinite',
+                bounce: 'bounce 2s linear infinite',
+                spin: 'spin 3s linear infinite'
+            },
+            transitionTimingFunction: {
+                'bounce': 'cubic-bezier(0.280, 0.840, 0.420, 1)'
+            },
+            keyframes: {
+                wiggle: {
+                    '0%, 100%': { transform: 'rotate(-15deg)' },
+                    '50%': { transform: 'rotate(15deg)' },
+                },
+                bounce: {
+                    '0%': { transform: 'scale(1, 1)      translateY(0)' },
+                    '10%': { transform: 'scale(1.1, .9)   translateY(0)' },
+                    '30%': { transform: 'scale(.9, 1.1)   translateY(-35%)' },
+                    '50%': { transform: 'scale(1.05, .95) translateY(0)' },
+                    '57%': { transform: 'scale(1, 1)      translateY(-5px)' },
+                    '64%': { transform: 'scale(1, 1)      translateY(0)' },
+                    '100%': { transform: 'scale(1, 1)      translateY(0)' },
+                },
+                spin: {
+                    'from': { transform: 'rotate(0deg)' },
+                    'to': { transform: 'rotate(-360deg)' }
+                }
+            }
         },
     },
     plugins: [daisyui],
