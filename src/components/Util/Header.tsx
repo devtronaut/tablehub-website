6f392efcb48cbfb0e129214919b42866230bd7b8
devtronaut <69691214+devtronaut@users.noreply.github.com>
2024-11-03 13:02:29 +0100

    Feature/translations (#7)
    * feature(translations): improve french and italian translations
    
    * feature(translations): fix translations
    
    * feature(demo): update tablehub version, implement resulting changes
    
    * chore: upgrade tablehub
    
    * fix: push header to foreground
    
    ---------
    
    Co-authored-by: Fabian Diemand <fabian.diemand@swisscom.com>

--- src/components/Util/Header.tsx
+++ src/components/Util/Header.tsx
@@ -5,7 +5,7 @@ import { Navbar } from './Navbar'
 
 export default function Header() {
     return (
-        <div className="min-w-full p-8 fixed top-0 left-0 flex flex-row justify-between items-center bg-backgroundLight/50 dark:bg-backgroundDark/50 backdrop-blur-md z-10">
+        <div className="min-w-full p-8 fixed top-0 left-0 flex flex-row justify-between items-center bg-backgroundLight/50 dark:bg-backgroundDark/50 backdrop-blur-md z-50">
             <div className="h-fit w-fit flex flex-row">
                 <a href="#home">
                     <Title size="md" />
